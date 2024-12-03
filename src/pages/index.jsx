b840f752e82ec9ec6b84c9561860d0f4064b6523
keimatsura <kei24abc@gmail.com>
2024-12-03 19:10:41 +0900

    ✨ 説明用のuseEffectを導入

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,8 @@
-
-
 import localFont from "next/font/local";
 import Footer from "src/components/Footer";
 import Header from "src/components/Header";
 import Main from "src/components/Main";
-
+import { useEffect } from "react";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -18,18 +16,19 @@ const geistMono = localFont({
 });
 
 export default function Home() {
+  useEffect(() => {
+    document.body.style.backgroundColor = "lightblue";
+    return () => {
+      document.body.style.backgroundColor = "";
+    };
+  }, []);
+
   return (
     <div
       className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
     >
-
       <Header />
-     
-
-
-      <Main
-        page="index"
-      />
+      <Main page="index" />
       <Footer />
     </div>
   );
